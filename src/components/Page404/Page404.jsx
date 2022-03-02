6b965a6f4c3aacf49129081e867a734fec07f489
Nam Duong <duongquocnam224400@gmail.com>
2022-03-02 15:45:31 +0700

    Updated-Page404

--- src/components/Page404/Page404.jsx
+++ src/components/Page404/Page404.jsx
@@ -1,7 +1,77 @@
-import React from 'react';
-
+import React, { Fragment } from 'react';
 const Page404 = () => {
-  return <div className="text-success">Hello Word</div>;
+  return (
+    <Fragment>
+      <div className="wrapper">
+        <div className="text">
+          <p>404</p>
+        </div>
+        <div className="container">
+          {/* caveman left */}
+          <div className="caveman">
+            <div className="leg">
+              <div className="foot">
+                <div className="fingers" />
+              </div>
+            </div>
+            <div className="leg">
+              <div className="foot">
+                <div className="fingers" />
+              </div>
+            </div>
+            <div className="shape">
+              <div className="circle" />
+              <div className="circle" />
+            </div>
+            <div className="head">
+              <div className="eye">
+                <div className="nose" />
+              </div>
+              <div className="mouth" />
+            </div>
+            <div className="arm-right">
+              <div className="club" />
+            </div>
+          </div>
+          {/* caveman right */}
+          <div className="caveman">
+            <div className="leg">
+              <div className="foot">
+                <div className="fingers" />
+              </div>
+            </div>
+            <div className="leg">
+              <div className="foot">
+                <div className="fingers" />
+              </div>
+            </div>
+            <div className="shape">
+              <div className="circle" />
+              <div className="circle" />
+            </div>
+            <div className="head">
+              <div className="eye">
+                <div className="nose" />
+              </div>
+              <div className="mouth" />
+            </div>
+            <div className="arm-right">
+              <div className="club" />
+            </div>
+          </div>
+        </div>
+        <a
+          href="https://codepen.io/SofiaSergio/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div id="link">
+            <i className="fab fa-codepen" />
+          </div>
+        </a>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Page404;
