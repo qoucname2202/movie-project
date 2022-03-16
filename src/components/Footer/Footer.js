8bf8c1b6453ddf8c451605f227fadeb762262cbd
Nam Duong <duongquocnam224400@gmail.com>
2022-03-16 17:01:13 +0700

    Updated-Footer-80%-UI

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,306 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div>
+      <footer className="footer">
+        <div className="footer-infor">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-4 block-footer">
+                <h4 className="ttl-4">BOOK MY FILM</h4>
+                <p>
+                  <a href="!#">FAQ</a>
+                  <span>
+                    <a href="!#">Thỏa thuận sử dụng</a>
+                  </span>
+                </p>
+                <p>
+                  <a href="!#">Brand Guidelines</a>
+                  <span>
+                    <a href="!#">Chính sách bảo mật</a>
+                  </span>
+                </p>
+              </div>
+              <div className="col-md-4 block-footer logo-brand">
+                <h4 className="ttl-4">BUSSINES PARTNERS</h4>
+                <div className="row">
+                  <div className="col">
+                    <a href="#!">
+                      <img
+                        src="../images/cgv.png"
+                        alt="logo-cgv"
+                        className="img-fluid"
+                      />
+                    </a>
+                  </div>
+                  <div className="col">
+                    <a href="#!">
+                      <img
+                        src="../images/bhd.png"
+                        alt="logo-bhd"
+                        className="img-fluid"
+                      />
+                    </a>
+                  </div>
+                  <div className="col">
+                    <a href="#!">
+                      <img
+                        src="../images/galaxycine.png"
+                        alt="log0-galaxycine"
+                        className="img-fluid"
+                      />
+                    </a>
+                  </div>
+                  <div className="col">
+                    <a href="#!">
+                      <img
+                        src="../images/cinestar.png"
+                        alt="logo-cinestar"
+                        className="img-fluid"
+                      />
+                    </a>
+                  </div>
+                  <div className="col">
+                    <a href="#!">
+                      <img
+                        src="../images/megags.png"
+                        alt="logo-megags"
+                        className="img-fluid"
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col">
+                    <a href="#!">
+                      <img
+                        src="../images/bt.jpg"
+                        alt="logo-beta"
+                        className="img-fluid"
+                      />
+                    </a>
+                  </div>
+                  <div className="col">
+                    <a href="#!">
+                      <img
+                        src="../images/dongdacinema.png"
+                        alt="logo-dongdacinema"
+                        className="img-fluid"
+                      />
+                    </a>
+                  </div>
+                  <div className="col">
+                    <a href="#!">
+                      <img
+                        src="../images/TOUCH.png"
+                        alt="logo-touch"
+                        className="img-fluid"
+                      />
+                    </a>
+                  </div>
+                  <div className="col">
+                    <a href="#!">
+                      <img
+                        src="../images/cnx.jpg"
+                        alt="logo-cnx"
+                        className="img-fluid"
+                      />
+                    </a>
+                  </div>
+                  <div className="col">
+                    <a href="#!">
+                      <img
+                        src="../images/STARLIGHT.png"
+                        alt="logo-starlight"
+                        className="img-fluid"
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col">
+                    <a href="#!">
+                      <img
+                        src="../images/dcine.png"
+                        alt="logo-dcine"
+                        className="img-fluid"
+                      />
+                    </a>
+                  </div>
+                  <div className="col">
+                    <a href="#!">
+                      <img
+                        src="../images/lotte.png"
+                        alt="logo-lotte"
+                        className="img-fluid"
+                      />
+                    </a>
+                  </div>
+                  <div className="col">
+                    <a href="#!">
+                      <img
+                        src="../images/payoo.jpg"
+                        alt="logo-payoo"
+                        className="img-fluid"
+                      />
+                    </a>
+                  </div>
+                  <div className="col">
+                    <a href="#!">
+                      <img
+                        src="../images/zalopay_icon.png"
+                        alt="logo-zalopay"
+                        className="img-fluid"
+                      />
+                    </a>
+                  </div>
+                  <div className="col">
+                    <a href="#!">
+                      <img
+                        src="../images/AGRIBANK.png"
+                        alt="logo-agribank"
+                        className="img-fluid"
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col">
+                    <a href="#!">
+                      <img
+                        src="../images/VCB.png"
+                        alt="logo-vcb"
+                        className="img-fluid"
+                      />
+                    </a>
+                  </div>
+                  <div className="col">
+                    <a href="#!">
+                      <img
+                        src="../images/VIETTINBANK.png"
+                        alt="logo-viettinbank"
+                        className="img-fluid"
+                      />
+                    </a>
+                  </div>
+                  <div className="col">
+                    <a href="#!">
+                      <img
+                        src="../images/IVB.png"
+                        alt="logo-IVB"
+                        className="img-fluid"
+                      />
+                    </a>
+                  </div>
+                  <div className="col">
+                    <a href="#!">
+                      <img
+                        src="../images/123go.png"
+                        alt="logo-go"
+                        className="img-fluid"
+                      />
+                    </a>
+                  </div>
+                  <div className="col">
+                    <a href="#!">
+                      <img
+                        src="../images/laban.png"
+                        alt="logo-laban"
+                        className="img-fluid"
+                      />
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 block-footer">
+                <div className="row">
+                  <div className="col-md-6 social">
+                    <h4 className="ttl-4">MOBILE APP</h4>
+                    <a href="#!">
+                      <img
+                        src="../images/apple-logo.png"
+                        alt="logo-apple"
+                        className="img-fluid"
+                      />
+                    </a>
+                    <a href="#!">
+                      <img
+                        src="../images/android-logo.png"
+                        alt="logo-android"
+                        className="img-fluid"
+                      />
+                    </a>
+                  </div>
+                  <div className="col-md-6 social">
+                    <h4 className="ttl-4">SOCIAL MEDIA</h4>
+                    <a href="#!">
+                      <img
+                        src="../images/facebook-logo.png"
+                        alt="logo-facebook"
+                        className="img-fluid"
+                      />
+                    </a>
+                    <a href="#!">
+                      <img
+                        src="../images/zalo-logo.png"
+                        alt="logo-zalo"
+                        className="img-fluid"
+                      />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="f-infor-bottom">
+              <div className="row">
+                <div className="col-md-8">
+                  <h4 className="ttl-4">BOOK MY FILM - ĐỒ ÁN TỐT NGHIỆP</h4>
+                  <p>
+                    Address: 475A Điện Biên Phủ, Phường 25, Quận Bình Thạnh, TP.
+                    Hồ Chí Minh
+                  </p>
+                  <p>Phone (Hotline): 0779382202</p>
+                  <p>
+                    Email:
+                    <a
+                      href="mailto:duongquocnam224400@gmail.com"
+                      style={{ color: '#FB4226' }}
+                    >
+                      duongquocnam224400@gmail.com
+                    </a>
+                  </p>
+                  <p>
+                    Github:
+                    <a
+                      href="https://github.com/DQNam2202"
+                      style={{ color: '#FB4226' }}
+                    >
+                      DQNam2202
+                    </a>
+                  </p>
+                </div>
+                <div className="col-md-4 text-center mt-5">
+                  <a
+                    href="http://online.gov.vn/Home/WebDetails/62782"
+                    target="_blank"
+                    className="img-fluid"
+                    rel="noreferrer"
+                  >
+                    <img
+                      src="../images/logo_1.png"
+                      alt="bocongthuong"
+                      className="img-fluid"
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
