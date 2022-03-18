af7b857490e7517c4dfeecefce01691912ec3e78
Nam Duong <duongquocnam224400@gmail.com>
2022-03-18 18:04:31 +0700

    Updated-Profile

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
+import Profile from "./pages/Profile/Profile";
 // import Login from './pages/Login/Login';
 // import Register from "./pages/Register/Register";
 // import Page404 from './components/Page404/Page404';
-import HomeTemplated from "./templates/HomeTemplate/HomeTemplated";
+// import HomeTemplated from "./templates/HomeTemplate/HomeTemplated";
 
 function App() {
   return (
     <div className="App">
       {/* <Page404 /> */}
-      <HomeTemplated />
+      {/* <HomeTemplated /> */}
+      <Profile />
       {/* <Login /> */}
       {/* <Register /> */}
     </div>
